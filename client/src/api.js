6550c4c52e6f26d052f6e54e2f649cfc4dcbee8e
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-11-04 13:06:14 +0300

    deleted .env file and  made changes to api.js regarding the deployed URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,7 @@
 // src/api.js
 
 // Use the deployed backend URL
-// export const API_BASE_URL = 'https://prestige-property-app-2.onrender.com';
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:5555';
-
+export const API_BASE_URL = 'https://prestige-property-app-2.onrender.com';
 
 // Helper function to construct endpoint URLs
 export const apiEndpoint = (path) => `${API_BASE_URL}/${path}`;
